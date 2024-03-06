14b6d346123427663fc522d89e667ced87528ac1
akirafunakoshi.project-akira <akirafunakoshi.project-akira@outlook.jp>
2024-03-06 13:08:39 +0900

    ストリーミング対応

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 import dotenv from 'dotenv/config'
-
 import Anthropic from '@anthropic-ai/sdk'
 
 const anthropic = new Anthropic({
     apiKey: process.env.ANTHROPIC_API_KEY, // 環境変数から取得
 })
 
-async function main() {
-    const message = await anthropic.messages.create({
-        max_tokens: 1024,
-        messages: [{ role: 'user', content: 'Hello, Claude' }],
+try {
+    const messageStream = await anthropic.messages.stream({
+        messages: [{ role: 'user', content: 'Hello' }],
         model: 'claude-3-opus-20240229',
+        max_tokens: 1024,
     })
 
-    console.log(message.content)
+    messageStream.on('text', (text) => {
+        process.stdout.write(text + '')
+    })
+} catch (error) {
+    console.error('An error occurred while streaming messages:', error)
 }
-
-main()
